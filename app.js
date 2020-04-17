7e366479baf4e2d4f76a284956858da0bd0c213b
Bhanuprakash <thotabhanu03@gmail.com>
2020-04-17 16:15:59 -0500

    Final Commit

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require('dotenv');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 const app = express();
+dotenv.config({ path: '.env' })
 
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://dwstudent:dw12345@cluster0-enbsv.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.ATLAS_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -27,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
